55891e30a8367b8582465569d2db824c20f813be
Louis Buckalew <lwb0003@auburn.edu>
2015-01-21 01:20:27 -0600

    animations

--- ytlooper/js/ytl-backbone.js
+++ ytlooper/js/ytl-backbone.js
@@ -17,31 +17,61 @@ $(document).ready(function() {
     var EmptyLoopView = Backbone.Marionette.ItemView.extend({
         template: '#no-loops-tpl',
         tagName: 'li',
-        className: 'ytloop collection-item row'
+        className: 'ytloopform collection-item row'
     });
 
     var LoopView = Backbone.Marionette.ItemView.extend({
         template: '#loop-tpl',
         tagName: 'li',
-        className: 'ytloop collection-item row',
+        className: 'ytloop collection-item row valign-wrapper',
         events: {
-            'mouseover' : 'mouseOn',
-            'mouseout' : 'mouseOff',
+            'mouseenter' : 'mouseOn',
+            'mouseleave' : 'mouseOff',
             'click .start' : 'asgnStart',
             'click .end' : 'asgnEnd',
             'click .loop-action' : 'loopit'
         },
         mouseOn: function() {
-            this.$el.toggleClass('z-depth-4');
-            this.$el.find('.loop-action').removeClass('disabled').find('i').addClass('mdi-navigation-refresh');
+            this.$el.toggleClass('z-depth-8');
+            this.$el.find('.loop-action').removeClass('disabled').find('i')
+            .animate({opacity: 0}, {
+                duration: 200,
+                step: function(now, fx) {
+                    var a = (1-now)*360;
+                    $(this).css('-webkit-transform','rotate('+a+'deg)');
+                },
+                done: function() {
+                    $(this).addClass('mdi-navigation-refresh');
+            }}).animate({opacity: 1}, {
+                duration: 200,
+                step: function(now, fx) {
+                    var a = (now)*360;
+                    $(this).css('-webkit-transform','rotate('+a+'deg)');
+            }});
             this.$el.find('.start.time i').addClass('green-text');
             this.$el.find('.end.time i').addClass('red-text');
+            this.$el.find('.loop-time-tag').slideDown({queue:false});
         },
         mouseOff: function() {
-            this.$el.toggleClass('z-depth-4');
-            this.$el.find('.loop-action').addClass('disabled').find('i').removeClass('mdi-navigation-refresh');
+            this.$el.toggleClass('z-depth-8');
+            this.$el.find('.loop-action').addClass('disabled').find('i')
+            .animate({opacity: 0}, {
+                duration: 200,
+                step: function(now, fx) {
+                    var a = (1-now)*360;
+                    $(this).css('-webkit-transform','rotate('+a+'deg)');
+                },
+                done: function() {
+                    $(this).removeClass('mdi-navigation-refresh');
+            }}).animate({opacity: 1}, {
+                duration: 200,
+                step: function(now, fx) {
+                    var a = (now)*360;
+                    $(this).css('-webkit-transform','rotate('+a+'deg)');
+            }});
             this.$el.find('.start.time i').removeClass('green-text');
             this.$el.find('.end.time i').removeClass('red-text');
+            this.$el.find('.loop-time-tag').slideUp({queue:false});
         },
         asgnStart: function() {
             var time = App.ytloop.getVidTime();
@@ -102,27 +132,38 @@ $(document).ready(function() {
         },
         ui: {
             urlInput: '#url_form input#url',
-            playButton: 'a#ctrl-play',
-            pauseButton: 'a#ctrl-pause',
+            ppButton: 'a#ctrl-pp',
             newButton: 'a#ctrl-add'
         },
         events: {
             'input @ui.urlInput' : 'inputURL',
-            'click @ui.playButton' : 'playVid',
-            'click @ui.pauseButton' : 'pauseVid',
+            'click @ui.urlInput' : 'clearURL',
+            'click @ui.ppButton' : 'ppVid',
             'click @ui.newButton' : 'newLoop',
             'keydown' : 'processKey'
         },
         initialize: function() {
             this.render();
-            // this.ui.playButton.hide();
-            // this.ui.pauseButton.hide();
+        },
+        clearURL: function(e) {
+            e.preventDefault();
+            this.ui.urlInput.val('').focus();
+        },
+        ppVid: function() {
+            if (App.player) {
+                var state = App.player.getPlayerState();
+                if (state==1) {
+                    this.pauseVid();
+                } else {
+                    this.playVid();
+                };
+            };
         },
         processKey: function(e) {
             if ($(':focus').length == 0) {
                 if (e.keyCode == 32) {
                     e.preventDefault();
-                    this.ui.playButton.add(this.ui.pauseButton).find(':visible').click();
+                    this.ui.ppButton.click();
                 }
             }
         },
@@ -132,6 +173,8 @@ $(document).ready(function() {
             var ytid = re.exec(input.val());
 
             if (ytid) {
+                $('iframe').detach();
+                if (App.player) {delete App.player;};
                 this.insertIframe(ytid[1]);
                 input.blur();
             } else {
@@ -180,7 +223,6 @@ $(document).ready(function() {
             App.ytloop.setVidTime(start);
             App.ytloop.playVid();
             App.loopTimeout = setTimeout(function(){that.loopVid(start,end)}, delta*1000);
-            console.log(App.loopTimeout)
         },
         newLoop: function() {
             this.$el.find('.ytloopform').detach();
@@ -193,6 +235,7 @@ $(document).ready(function() {
                 loop.set({YTid:this.ui.urlInput.val()});
                 App.loops.add(loop);
             } else {
+                this.clearURL();
                 alert('Need to enter a YouTube URL');
             }
         },
@@ -206,15 +249,42 @@ $(document).ready(function() {
     };
     onPlayerStateChange = function() {
         var state = App.player.getPlayerState();
+        App.ytloop.ui.ppButton.find('i').clearQueue();
         if (state == 1) {
-            App.ytloop.ui.playButton.hide();
-            App.ytloop.ui.pauseButton.show();
+            App.ytloop.ui.ppButton.find('i')
+            .animate({opacity:0}, {
+                step: function(now, fx) {
+                    var a = (1-now)*360;
+                    $(this).css('-webkit-transform','rotate('+a+'deg)');
+                },
+                done: function() {
+                    $(this).removeClass('mdi-av-play-arrow');
+                }
+            }).animate({opacity:1}, {
+                step: function(now, fx) {
+                    var a = (now)*360;
+                    $(this).css('-webkit-transform','rotate('+a+'deg)');
+                }
+            });
         } else {
             if (state == 2) {
                 App.ytloop.clearTimeout();
             };
-            App.ytloop.ui.playButton.show();
-            App.ytloop.ui.pauseButton.hide();
+            App.ytloop.ui.ppButton.find('i')
+            .animate({opacity:0}, {
+                step: function(now, fx) {
+                    var a = (1-now)*360;
+                    $(this).css('-webkit-transform','rotate('+a+'deg)');
+                },
+                done: function() {
+                    $(this).addClass('mdi-av-play-arrow');
+                }
+            }).animate({opacity:1}, {
+                step: function(now, fx) {
+                    var a = (now)*360;
+                    $(this).css('-webkit-transform','rotate('+a+'deg)');
+                }
+            });
         };
     };
 
